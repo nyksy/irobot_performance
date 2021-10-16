92ed4ed9aeda7c4051f0c11db7f927c13147e40d
nyksy <71010049+nyksy@users.noreply.github.com>
2021-10-16 14:26:39 +0300

    initial

---
+++ controllers/my_controller/build/release/my_controller.d
@@ -0,0 +1,4 @@
+build/release/my_controller.o: my_controller.c \
+ C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
