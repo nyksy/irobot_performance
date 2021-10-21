0d6d291ef3d96c71b79f7d290a9c77d59575fd3c
nyksy <71010049+nyksy@users.noreply.github.com>
2021-10-21 21:02:04 +0300

    controllerin rakennusta

--- controllers/ground/build/release/ground.d
+++
@@ -1,7 +0,0 @@
-build/release/ground.o: ground.c \
- C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/display.h \
- C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/supervisor.h \
- C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/contact_point.h
--- controllers/my_controller/build/release/my_controller.d
+++
@@ -1,4 +0,0 @@
-build/release/my_controller.o: my_controller.c \
- C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\Juho-Main\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
