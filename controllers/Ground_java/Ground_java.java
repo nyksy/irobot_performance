9a206d7b4eee7647da5d7210acf6f3045bebc68e
Teppo Pitkänen <tpitkane@student.uef.fi>
2021-10-28 00:44:06 +0300

    Ground java

---
+++ controllers/Ground_java/Ground_java.java
@@ -0,0 +1,63 @@
+// File:          Ground_java.java
+// Date:
+// Description:
+// Author:
+// Modifications:
+
+// You may need to add other webots classes
+import com.cyberbotics.webots.controller.*;
+
+// This class defines how to initialize and how to run your controller.
+public class Ground_java {
+
+  static final int TIME_STEP = 64;
+
+  static final int X = 0;
+  static final int Z = 2;
+
+  // size of the ground
+  static final double GROUND_X = 4.9;
+  static final double GROUND_Z = 4.9;
+
+  // Main function of the controller.
+  public static void main(String[] args) {
+
+    Supervisor supervisor = new Supervisor();
+
+    // First we get a handler to devices
+    Display display = supervisor.getDisplay("ground_display");
+
+    // get the properties of the Display
+    int width = display.getWidth();
+    int height = display.getHeight();
+
+    // prepare stuff to get the
+    // Robot(IROBOT_CREATE).translation field
+    Node mybot = supervisor.getFromDef("IROBOT_CREATE");
+    Field translationField = mybot.getField("translation");
+
+    // set the background (otherwise an empty ground is displayed at this step)
+    ImageRef background = display.imageLoad("../../worlds/textures/dirty.jpg");
+    display.imagePaste(background, 0, 0, false);
+
+    // set the pen to remove the texture
+    display.setAlpha(0.0);
+
+    // Main loop:
+    // - perform simulation steps until Webots is stopping the controller
+    while (supervisor.step(TIME_STEP) != -1) {
+
+      // Update the translation field
+      double[] translation = translationField.getSFVec3f();
+
+      int intX = (int) (width * (translation[X] + GROUND_X / 2) / GROUND_X);
+      int intZ = (int) (height * (translation[Z] + GROUND_Z / 2) / GROUND_Z);
+
+      // display the robot position
+      display.fillOval(intX, intZ, 14, 14);
+
+    };
+
+    // Enter here exit cleanup code.
+  }
+}
